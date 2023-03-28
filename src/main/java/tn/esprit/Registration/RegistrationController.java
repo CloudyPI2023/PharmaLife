236a6222fb189a35e757401bc1544226c0cafa0d
emna-hannachii <emna.hannachi@esprit.tn>
2023-03-28 21:00:45 +0100

    Registration Backend + Email Verification Done

--- src/main/java/tn/esprit/Registration/RegistrationController.java
+++ src/main/java/tn/esprit/Registration/RegistrationController.java
@@ -2,41 +2,19 @@ package tn.esprit.Registration;
 
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
-import tn.esprit.Entities.User;
-import tn.esprit.Services.UserService;
 
-import javax.validation.constraints.Email;
 
 @RestController
-@RequestMapping(path="api/v1/registration")
+@RequestMapping(path="/registration")
 @AllArgsConstructor
 public class RegistrationController {
 
-    private  final UserService userService;
     private RegistrationService registrationService;
-    private EmailValidator emailValidator;
 
     @PostMapping
     public String register(@RequestBody RegistrationRequest request) {
-        boolean isValidEmail =  emailValidator.test(request.getEmail());
-        if(!isValidEmail) {
-            throw new IllegalStateException("email not valid");
-        }
+      return registrationService.register(request);
 
-        return userService.signUpUser(
-                new User(
-                        request.getFirstName(),
-                        request.getLastName(),
-                        request.getEmail(),
-                        request.getPhoneNumber(),
-                        request.getBirthDate(),
-                        request.getAddress(),
-                        request.getCity(),
-                        request.getPassword(),
-                        request.getRole(),
-                        request.getGender()
-                )
-        );
 
     }
 
