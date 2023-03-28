2a0b6a5e28aeb04d7606f760639d247893c3533d
emna-hannachii <emna.hannachi@esprit.tn>
2023-03-28 02:36:49 +0100

    registration and spring security in progress

--- src/main/java/tn/esprit/Entities/User.java
+++ src/main/java/tn/esprit/Entities/User.java
@@ -3,15 +3,23 @@ package tn.esprit.Entities;
 
 import javax.validation.constraints.*;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
-import org.springframework.web.bind.annotation.CrossOrigin;
+import org.hibernate.annotations.Formula;
+import org.hibernate.validator.constraints.Length;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 
 import javax.persistence.*;
 import java.io.Serializable;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
-
+import java.time.Period;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.Date;
 import java.util.Set;
 
@@ -23,50 +31,93 @@ import java.util.Set;
 @NoArgsConstructor
 @Entity
 @Table( name = "User")
-@CrossOrigin
-public class User implements Serializable{
+public class User implements Serializable, UserDetails {
 
+
+    private static final long serialVersionUID =1L;
+    @SequenceGenerator(
+            name = "user_sequence",
+            sequenceName = "user_sequence",
+            allocationSize = 1
+    )
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "user_sequence"
+    )
+
+    // @Id
+    // @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name="idUser")
     private Integer idUser;
 
+
+    @NotNull(message = "First Name cannot be empty")
+    @Column(length=50, nullable=false, unique=false)
     private String firstName;
 
+
+    @Column(length=50, nullable=false, unique=false)
+    @NotNull(message = "Last Name cannot be empty")
     private String lastName;
 
+
+    @NotNull(message = "Email cannot be empty")
+    @Email(message = "Please enter a valid email address")
+    @Column(unique = true,nullable=false)
     private String email;
 
+
+    @NotNull(message = "Phone number cannot be empty")
+    @Pattern(regexp = "\\d{8}", message = "Phone number must be 8 digits")
+    @Column(nullable=false)
     private String phoneNumber;
 
 
+    @Transient
+    // @Formula("YEAR(CURDATE()) - YEAR(BIRTHDATE)")
+    private Integer age;
+
+    // @Temporal(TemporalType.DATE)
+    private LocalDate birthDate;
+
+    private String imageUser;
+
+
+    @NotNull(message = "Address cannot be empty")
+    @Column(nullable=false)
     private String address;
 
 
+    @NotNull(message = "City cannot be empty")
+    @Column(nullable=false)
     private String city;
 
 
+    @NotNull(message = "Password cannot be empty")
+    @Length(min = 7,max=50, message = "Password should be atleast 7 characters long")
+    @Column(nullable=false)
     private String password;
 
-    @Transient
-    private Integer age;
-
-    @Temporal(TemporalType.DATE)
-    private Date birthDate;
 
 
     @CreationTimestamp
     @Column(updatable = false,nullable=true)
     private LocalDateTime createdAt;
 
+    private Boolean locked = false;
+
+    private Boolean enabled = false;
+
 
 
     @Enumerated(EnumType.STRING)
+    @Column(nullable=false)
     private Role role;
-
 
     @Enumerated(EnumType.STRING)
     private Gender gender;
+
 
     //Associations
 
@@ -84,4 +135,41 @@ public class User implements Serializable{
 
 
 
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        SimpleGrantedAuthority authority =
+                new SimpleGrantedAuthority(role.name());
+        return Collections.singletonList(authority);
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return !locked;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return enabled;
+    }
 }
--- src/main/java/tn/esprit/Services/UserService.java
+++ src/main/java/tn/esprit/Services/UserService.java
@@ -3,6 +3,9 @@ package tn.esprit.Services;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import tn.esprit.Entities.Role;
 import tn.esprit.Entities.User;
@@ -14,9 +17,12 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
-public class UserService implements IUserService {
+public class UserService implements IUserService, UserDetailsService {
 
+    private final static String USER_NOT_FOUND_MSG =
+            "user with email %s not found";
 
+     @Autowired
     private final UserRepository userRepository;
 
 
@@ -28,10 +34,9 @@ public class UserService implements IUserService {
 
     @Override
     public List<User> getUsers() {
-       return userRepository.findAll();
+        // return userRepository.findAll(Sort.by(Sort.Direction.DESC, "firstName"));
+        return userRepository.findAll();
     }
-
-
 
     @Override
     public User findByEmail(String email) {
@@ -43,6 +48,12 @@ public class UserService implements IUserService {
         return userRepository.findById(id).orElse(null);
     }
 
+
+  /*  @Override
+    public void deleteUserByEmail(String email) {
+        User u = userRepository.findByEmail(email).orElse(null);
+        userRepository.delete(u);
+    }*/
 
     @Override
     public void deleteUserById(Integer id) {
@@ -74,6 +85,12 @@ public class UserService implements IUserService {
     }
 
 
-
-
+    @Override
+    public UserDetails loadUserByUsername(String email)
+            throws UsernameNotFoundException {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException(
+                                String.format(USER_NOT_FOUND_MSG, email)));
+    }
 }
