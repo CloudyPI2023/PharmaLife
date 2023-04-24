d6bb514d93a0c0b5e0ee4c0854b795837172305a
mouna-ziadi <mouna.ziadi@esprit.tn>
2023-04-24 01:43:15 +0100

    donation controller

--- src/main/java/tn/esprit/Controllers/DonationRestController.java
+++ src/main/java/tn/esprit/Controllers/DonationRestController.java
@@ -5,14 +5,17 @@ import org.springframework.web.bind.annotation.*;
 import tn.esprit.Entities.Donation;
 import tn.esprit.Services.IDonationService;
 
+import java.util.HashMap;
 import java.util.List;
 
 @RestController
-@AllArgsConstructor
 @CrossOrigin("*")
+@AllArgsConstructor
 @RequestMapping("/donations")
+
 public class DonationRestController {
     IDonationService iDonationService;
+
     @PostMapping("/addDonation")
     public Donation addDonation(@RequestBody Donation d){
         Donation donation = iDonationService.addDonation(d);
@@ -30,6 +33,14 @@ public class DonationRestController {
         iDonationService.deleteDonation(IdDonation);
     }
 
+    @GetMapping("/getMyDonations/{idUser}")
+    public List<Donation> getDonationsByUser (@PathVariable("idUser") Integer idUser)
+    {
+        return iDonationService.retrieveMyDonations(idUser);
+
+    }
+
+
     @GetMapping("/retrieveAllDonations")
     public List<Donation> retrieveAllDonations(){
         List<Donation> listDonations = iDonationService.retrieveAllDonations();
@@ -38,7 +49,12 @@ public class DonationRestController {
 
     @GetMapping("/retrieveDonation/{id_donation}")
     public Donation retrieveDonation(@PathVariable("id_donation")Integer IdDonation){
-        return iDonationService.RetrieveDonation(IdDonation);
+        return iDonationService.retrieveDonation(IdDonation);
+    }
+
+    @GetMapping("/statisticsDonationStatus")
+    HashMap<String, Integer> DonationsByStatus(){
+        return iDonationService.DonationByStatus();
     }
 
 }
