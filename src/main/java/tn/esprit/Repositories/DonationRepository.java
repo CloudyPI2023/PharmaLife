65acf06e2682d172d3c34a1d7ff71b4a7ec63af7
mouna-ziadi <mouna.ziadi@esprit.tn>
2023-04-24 01:39:57 +0100

    fix the donation repo methodes

--- src/main/java/tn/esprit/Repositories/DonationRepository.java
+++ src/main/java/tn/esprit/Repositories/DonationRepository.java
@@ -2,11 +2,17 @@ package tn.esprit.Repositories;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import tn.esprit.Entities.Donation;
 import tn.esprit.Entities.User;
 
 import java.util.List;
 
 public interface DonationRepository extends CrudRepository<Donation, Integer> {
+    @Query("select d from Donation d inner join d.userDonation ee where ee.idUser = ?1")
+    List<Donation> getDonationsByUserDonation (Integer idUser);
+
+    @Query("SELECT r FROM Donation r WHERE r.userDonation.idUser =:user")
+    User findDonationByUserDonation (@Param("user") int user);
 
 }
