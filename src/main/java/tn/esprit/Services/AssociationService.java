7d729e9ca6465074b164d9534f3f8c2c9afb3cea
mouna-ziadi <mouna.ziadi@esprit.tn>
2023-04-24 01:36:17 +0100

    email

--- src/main/java/tn/esprit/Services/AssociationService.java
+++ src/main/java/tn/esprit/Services/AssociationService.java
@@ -1,21 +1,45 @@
 package tn.esprit.Services;
 
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 import tn.esprit.Entities.Association;
 import tn.esprit.Repositories.AssociationRepository;
 
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 @Service
 @AllArgsConstructor
 public class AssociationService implements IAssociationService {
 
+    @Autowired
     AssociationRepository associationRepository;
+    ///email
+    @Autowired
+    private JavaMailSender javaMailSender;
+
     @Override
-    public Association addAssociation(Association a) {
-        return associationRepository.save(a);
+    public String addAssociationByMail(Association a) {
+        Association u = associationRepository.findByEmail(a.getEmailAssocation());
+        if(u == null) {
+
+            associationRepository.save(a);
+
+            this.sendSimpleMail(a.getEmailAssocation());
+            return "Asso added successfully";
+        }
+        else {
+            return "Email does not exist";
+        }
     }
+
+
 
     @Override
     public Association updateAssociation(Association a) {
@@ -28,12 +52,35 @@ public class AssociationService implements IAssociationService {
     }
 
     @Override
-    public List<Association> retrieveAllAssociations() {
-        return (List<Association>) associationRepository.findAll();
+    public String sendSimpleMail(String email) {
+        // Try block to check for exceptions
+        try {
+
+            // Creating a simple mail message
+            SimpleMailMessage mailMessage = new SimpleMailMessage();
+
+            // Setting up necessary details
+            mailMessage.setFrom("cloudypi2023@gmail.com");
+            mailMessage.setTo(email);
+            mailMessage.setText("Congratulation!!\u0020\nYour Association on PharmaLIfe was successfully added on " + LocalDate.now());
+            mailMessage.setSubject("CONFIRMATION");
+
+            // Sending the mail
+            javaMailSender.send(mailMessage);
+            return "Mail Sent Successfully...";
+        }
+
+        // Catch block to handle the exceptions
+        catch (Exception e) {
+            return "Error while Sending Mail";
+        }
     }
 
     @Override
-    public Association RetrieveAssociation(Integer idAssociation) {
+    public Association retrieveAssociation(Integer idAssociation) {
         return associationRepository.findById(idAssociation).get();
     }
+
+
+
 }
--- src/main/java/tn/esprit/Services/IAssociationService.java
+++ src/main/java/tn/esprit/Services/IAssociationService.java
@@ -2,15 +2,35 @@ package tn.esprit.Services;
 
 import tn.esprit.Entities.Association;
 
+
+import java.util.HashMap;
 import java.util.List;
 
 public interface IAssociationService {
 
+    public String addAssociationByMail (Association a);
     Association addAssociation (Association a);
     Association updateAssociation (Association a);
     void deleteAssociation (Integer idAssociation);
 
-    Association RetrieveAssociation(Integer idAssociation);
+
     List<Association> retrieveAllAssociations();
+    //recherche Dynamique
+
+
+    /////email
+    public String sendSimpleMail(String email);
+
+    Association retrieveAssociation(Integer idAssociation);
+
+
+    HashMap<String, Integer> nombreAnneeParAssociation();
+
+    //Integer nbAnnes(Integer idAssociation);
+
+    List<Association> getAssociationsPlusDeDeuxAns();
+
+    void calculerNombreAnnees();
+
 
 }
