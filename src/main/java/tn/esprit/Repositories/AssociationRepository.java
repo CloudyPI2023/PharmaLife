1c514fc7bc2efcfb3d18b3efccd77f1f76a456e3
mouna-ziadi <mouna.ziadi@esprit.tn>
2023-04-24 01:32:04 +0100

    get nb of years

--- src/main/java/tn/esprit/Repositories/AssociationRepository.java
+++ src/main/java/tn/esprit/Repositories/AssociationRepository.java
@@ -1,7 +1,16 @@
 package tn.esprit.Repositories;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import tn.esprit.Entities.Association;
+import tn.esprit.Entities.Request;
+
+import java.util.List;
 
 public interface AssociationRepository extends CrudRepository<Association,Integer> {
+
+
+    @Query("SELECT DATEDIFF(CURRENT_DATE, a.DateAssociation) / 365 FROM Association a WHERE a.idAssociation = :id_association")
+    Integer getAssociationByDateAssociation_Year(@Param("id_association") Integer id_association);
 }
