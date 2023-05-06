87f448dc155a6f00026283f8cd393d4b0d47d94f
mouna-ziadi <mouna.ziadi@esprit.tn>
2023-05-07 00:49:01 +0100

    Donation Service done

--- src/main/java/tn/esprit/Services/DonationService.java
+++ src/main/java/tn/esprit/Services/DonationService.java
@@ -4,9 +4,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import tn.esprit.Entities.*;
 import tn.esprit.Repositories.DonationRepository;
-import tn.esprit.Repositories.RequestRepository;
 import tn.esprit.Repositories.UserRepository;
-
 import java.text.DateFormatSymbols;
 import java.time.LocalDate;
 import java.util.*;
@@ -60,29 +58,6 @@ public class DonationService implements IDonationService {
         return donationRepository.findById(idDonation).get();
     }
 
-
-    /*@Override
-    public HashMap<String, Float> DonationByStatus() {
-        HashMap<String, Float> map=new HashMap<>();
-        List<Donation> listDonations= (List<Donation>) donationRepository.findAll();
-        Integer nbDonations = listDonations.size();
-        for (Donation d:listDonations) {
-            String status = d.getStatusDonation().name();
-            if(map.containsKey(status)){
-               map.put(status,((map.get(status)+1)/nbDonations)*100);
-        //        map.put(status,(map.get(status)+1);
-
-            }
-            else {
-                map.put(status, (float) ((1/nbDonations)*100));
-           //     map.put(status,1);
-            }
-        }
-       // for( HashMap<String, Integer>  m :map){
-
-       // }
-        return map;
-    }*/
 
     public Map<String, Double> DonationByStatus() {
         List<Donation> donations = (List<Donation>) donationRepository.findAll();
@@ -143,32 +118,6 @@ public class DonationService implements IDonationService {
         return String.format("%02d", month);
     }
 
-  /* @Override
-    public Map<String, Map<String, Integer>> getDonationStatisticsByDate1() {
-       List<Donation> donations = (List<Donation>) donationRepository.findAll();
-        Map<String, Map<String, Integer>> donationStatistics = new HashMap<>();
-        for (Donation donation : donations) {
-            String monthYear = getMonthYearFromDonationDate1(Date.valueOf(donation.getDateDonation()));
-            String[] monthYearArray = monthYear.split("-");
-            String month = monthYearArray[0];
-            String year = monthYearArray[1];
-            if (donationStatistics.containsKey(year)) {
-                Map<String, Integer> yearStatistics = donationStatistics.get(year);
-                if (yearStatistics.containsKey(month)) {
-                    yearStatistics.put(month, yearStatistics.get(month) + 1);
-                } else {
-                    yearStatistics.put(month, 1);
-                }
-            } else {
-                Map<String, Integer> yearStatistics = new HashMap<>();
-                yearStatistics.put(month, 1);
-                donationStatistics.put(year, yearStatistics);
-            }
-        }
-        return donationStatistics;
-    }*/
-
-
     @Override
     public Map<String, Map<String, Integer>> getDonationStatisticsByDate1() {
         List<Donation> donations = (List<Donation>) donationRepository.findAll();
@@ -206,16 +155,6 @@ public class DonationService implements IDonationService {
         String monthName = months[month];
         return monthName + "-" + year;
     }
-
-
-    /*private String getMonthYearFromDonationDate1(Date donationDate) {
-        Calendar calendar = Calendar.getInstance();
-        calendar.setTime(donationDate);
-        int month = calendar.get(Calendar.MONTH) + 1;
-        int year = calendar.get(Calendar.YEAR);
-        return String.format("%02d-%d", month, year);
-    }*/
-
 
 
 
--- src/main/java/tn/esprit/Services/IDonationService.java
+++ src/main/java/tn/esprit/Services/IDonationService.java
@@ -6,7 +6,7 @@ import java.util.List;
 import java.util.Map;
 
 public interface IDonationService {
-    //ces fonctionnalités sont réalisés par 'Donator'
+
     Donation addDonation (Donation d);
     Donation updateDonation (Donation d);
     void deleteDonation (Integer idDonation);
@@ -15,7 +15,6 @@ public interface IDonationService {
 
 
 
-    //ces fonctionnalités sont réalisées par 'Admin'
     List<Donation> retrieveAllDonations();
     Donation retrieveDonation(Integer idDonation);
 
@@ -23,7 +22,7 @@ public interface IDonationService {
 
     Map<String, Double> DonationByType();
 
-    //recherche dynamique
+
 
     Map<String, Integer> getDonationStatisticsByDate();
 
